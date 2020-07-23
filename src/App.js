fec9b92eb3bf5aaf3128c06380032ec506e134c7
sagarbajpai <ashversache@gmail.com>
2020-07-23 23:15:32 +0530

    Hooks are ready

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { createContext } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import CounterWithClass from "./Components/Classes";
 import CounterWithHook from "./Components/Hooks";
 import Index from "./Components/Index";
+
+export const AppContext = createContext(); //Creating a context to pass. can be anything object, string!
+const { Provider } = AppContext;
+
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Index}></Route>
-        <Route exact path="/class" component={CounterWithClass}></Route>
-        <Route exact path="/hook" component={CounterWithHook}></Route>
-      </Switch>
+      <Provider value={{ name: "Real Dev Squad" }}>
+        <Switch>
+          <Route exact path="/" component={Index}></Route>
+          <Route exact path="/hook" component={CounterWithHook}></Route>
+        </Switch>
+      </Provider>
     </Router>
   );
 }
 
 export default App;
+
+// Lets say we want to pass down the default websiteName color to component tree
+// CREATE a context
+// Context will return APIs: Provider and Consumer
--- src/Components/Hooks.js
+++ src/Components/Hooks.js
@@ -1,34 +1,36 @@
-import React, { useEffect, useState } from "react";
-import { useFetch } from "./Hooks/useFetch";
+import React, { useState } from "react";
+import { AppContext } from "../App";
 
 const CounterOnceAgain = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [count, setCount] = useState(0);
 
-  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia?`);
-
-  useEffect(() => {}, []);
-
   return (
-    <div>
-      <div>{loading ? "loading..." : data}</div>
-      <button onClick={() => setCount((count) => count + 1)}>
-        Increment Me
-      </button>
-      <input
-        type="text"
-        placeholder="name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      ></input>
-      <input
-        type="password"
-        placeholder="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      ></input>
-    </div>
+    <AppContext.Consumer>
+      {(props) => {
+        return (
+          <div>
+            {count}
+            <button onClick={() => setCount((count) => count + 1)}>
+              {props.name}
+            </button>
+            <input
+              type="text"
+              placeholder="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+            <input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+          </div>
+        );
+      }}
+    </AppContext.Consumer>
   );
 };
 
