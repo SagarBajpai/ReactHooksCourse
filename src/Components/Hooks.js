4659d7802a380d816b8a4bedef3b7f3b10cc0302
sagarbajpai <ashversache@gmail.com>
2020-07-24 13:15:00 +0530

    useReducer implemented

--- src/Components/Hooks.js
+++ src/Components/Hooks.js
@@ -1,38 +1,67 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useReducer } from "react";
 import { AppContext } from "../App";
+
+const initialState = {
+  count: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREASE_COUNT":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "DECREASE_COUNT":
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    default:
+      return state;
+  }
+};
 
 const CounterOnceAgain = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
-  const [count, setCount] = useState(0);
-
-  //const data = useContext(AppContext); // Use Hooks if you wants to avoid wrapping your component
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const data = useContext(AppContext); // Use Hooks if you wants to avoid wrapping your component
 
   return (
-    <AppContext.Consumer>
-      {(props) => {
-        return (
-          <div>
-            {count}
-            <button onClick={() => setCount((count) => count + 1)}>
-              {props.name}
-            </button>
-            <input
-              type="text"
-              placeholder="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            ></input>
-            <input
-              type="password"
-              placeholder="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            ></input>
-          </div>
-        );
-      }}
-    </AppContext.Consumer>
+    <div>
+      <div>{state.count}</div>
+      <button
+        onClick={() =>
+          dispatch({
+            type: "INCREASE_COUNT",
+          })
+        }
+      >
+        {"Increase"}
+      </button>
+      <button
+        onClick={() =>
+          dispatch({
+            type: "DECREASE_COUNT",
+          })
+        }
+      >
+        {"Decrease"}
+      </button>
+      <input
+        type="text"
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      ></input>
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      ></input>
+    </div>
   );
 };
 
