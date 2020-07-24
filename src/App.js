de6d2b4c830e415aa0d5fa464629b230f49fac96
sagarbajpai <ashversache@gmail.com>
2020-07-24 13:31:13 +0530

    state management using context and useReducer is done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React, { createContext } from "react";
+import React, { createContext, useReducer } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CounterWithHook from "./Components/Hooks";
 import Index from "./Components/Index";
 
-export const AppContext = createContext(); //Creating a context to pass. can be anything object, string!
-const { Provider } = AppContext;
+export const StateContext = createContext(); //Creating a context to pass. can be anything object, string!
+export const DispatchContext = createContext();
+
+let intitialState = {
+  count: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "DECREMENT":
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, intitialState);
   return (
     <Router>
-      <Provider value={{ name: "Real Dev Squad" }}>
-        <Switch>
-          <Route exact path="/" component={Index}></Route>
-          <Route exact path="/hook" component={CounterWithHook}></Route>
-        </Switch>
-      </Provider>
+      <StateContext.Provider value={state}>
+        <DispatchContext.Provider value={dispatch}>
+          <Switch>
+            <Route exact path="/" component={Index}></Route>
+            <Route exact path="/hook" component={CounterWithHook}></Route>
+          </Switch>
+        </DispatchContext.Provider>
+      </StateContext.Provider>
     </Router>
   );
 }
--- src/Components/Hooks.js
+++ src/Components/Hooks.js
@@ -1,38 +1,49 @@
 import React, { useState, useContext } from "react";
-import { AppContext } from "../App";
+import { StateContext, DispatchContext } from "../App";
 
 const CounterOnceAgain = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
-  const [count, setCount] = useState(0);
+
+  const state = useContext(StateContext);
+  const dispatch = useContext(DispatchContext);
 
   //const data = useContext(AppContext); // Use Hooks if you wants to avoid wrapping your component
-
+  console.log("state : ", state);
   return (
-    <AppContext.Consumer>
-      {(props) => {
-        return (
-          <div>
-            {count}
-            <button onClick={() => setCount((count) => count + 1)}>
-              {props.name}
-            </button>
-            <input
-              type="text"
-              placeholder="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            ></input>
-            <input
-              type="password"
-              placeholder="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            ></input>
-          </div>
-        );
-      }}
-    </AppContext.Consumer>
+    <div>
+      <div>{state.count}</div>
+      <button
+        onClick={() =>
+          dispatch({
+            type: "INCREMENT",
+          })
+        }
+      >
+        {"Increase"}
+      </button>
+      <button
+        onClick={() =>
+          dispatch({
+            type: "DECREMENT",
+          })
+        }
+      >
+        {"Decrease"}
+      </button>
+      <input
+        type="text"
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      ></input>
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      ></input>
+    </div>
   );
 };
 
