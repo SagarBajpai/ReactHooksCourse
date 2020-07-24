a796f27b1961c0fb795d2ab161b27a369f754b6b
sagarbajpai <ashversache@gmail.com>
2020-07-24 16:44:58 +0530

    Comments are added

--- src/Components/Hooks.js
+++ src/Components/Hooks.js
@@ -12,3 +12,10 @@ const CounterOnceAgain = () => {
 };
 
 export default CounterOnceAgain;
+
+//Hooks are a new addition in React 16.8. They let you use state and other React features without writing a class.
+
+//Benefits
+//Can use states in functional component
+//Not have to use this keyword anymore
+//60% cleaner code
